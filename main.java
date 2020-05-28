b256948a65306a388cefd9792a82cc1673890561
Nunustolemybike <36703935+Nunustolemybike@users.noreply.github.com>
2020-05-28 11:29:50 +0200

    1.2

--- main.java
+++ main.java
@@ -1,3 +1,7 @@
+import junit.framework.*;
+
+import static org.junit.Assert.assertTrue;
+
 package main;
 
 public class main {
@@ -17,4 +21,12 @@ public class main {
         public SommeArgent add(SommeArgent m) {
             return new SommeArgent(getQuantite()+m.getQuantite(), getUnite());
         }
-}
+        public boolean equals(Object anObject){
+            SommeArgent S= new SommeArgent(5,"euros");
+            int quantite=S.getQuantite();
+            String unite=S.getUnite();
+            assertTrue(quantite==5);
+        }
+    }
+
+    }
